263d63930ab85bbff9884cde2f555e4fa8406c77
AnDrEs1482 <mejia.nunez2027@hilasal.superate.org.sv>
2026-04-09 21:01:56 -0600

    last commit

--- js/logic.js
+++ js/logic.js
@@ -1,22 +1,22 @@
    function verifyWithdrawal() {
-    let  pricePurchase= Number(document.getElementById("pricePurchase").value);
-    let haveDiscountCoupon = (document.getElementById("haveCoupon").value === "true");
+    let  pricePurchase= Number(document.getElementById("purchasePrice").value);
+    let hasDiscountCoupon = (document.getElementById("hasCoupon").value === "true");
 
     
 // if you have a discount coupon, you get a 40% of discount
 
-    if (haveDiscountCoupon === true && pricePurchase >= 50 && pricePurchase < 100) {
-        document.getElementById("totalAmount").textContent = pricePurchase - (pricePurchase * 0.5);
-    }else if (haveDiscountCoupon === true && pricePurchase >= 100) {
-        document.getElementById("totalAmount").textContent = pricePurchase - (pricePurchase * 0.6);
-    }else if (pricePurchase >= 100 && haveDiscountCoupon === false) {
-        document.getElementById("totalAmount").textContent =  pricePurchase - (pricePurchase * 0.2);
-    }else if (pricePurchase >= 50 && haveDiscountCoupon === false) {
-        document.getElementById("totalAmount").textContent = pricePurchase - (pricePurchase * 0.1);
-    }else if (haveDiscountCoupon === true) {
-        document.getElementById("totalAmount").textContent = pricePurchase - (pricePurchase * 0.4);
+    if (hasDiscountCoupon === true && pricePurchase >= 50 && pricePurchase < 100) {
+        document.getElementById("finalPrice").textContent = pricePurchase - (pricePurchase * 0.5);
+    }else if (hasDiscountCoupon === true && pricePurchase >= 100) {
+        document.getElementById("finalPrice").textContent = pricePurchase - (pricePurchase * 0.6);
+    }else if (pricePurchase >= 100 && hasDiscountCoupon === false) {
+        document.getElementById("finalPrice").textContent =  pricePurchase - (pricePurchase * 0.2);
+    }else if (pricePurchase >= 50 && hasDiscountCoupon === false) {
+        document.getElementById("finalPrice").textContent = pricePurchase - (pricePurchase * 0.1);
+    }else if (hasDiscountCoupon === true) {
+        document.getElementById("finalPrice").textContent = pricePurchase - (pricePurchase * 0.4);
     }else {
-        document.getElementById("totalAmount").textContent = pricePurchase;
+        document.getElementById("finalPrice").textContent = pricePurchase;
 
     }
 }
